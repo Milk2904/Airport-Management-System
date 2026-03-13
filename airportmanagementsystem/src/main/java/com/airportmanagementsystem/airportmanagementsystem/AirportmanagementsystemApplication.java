543b023a901ac85a8b11a528aec6008cc5291f9c
Milk2904 <vominhdat65@gmail.com>
2026-03-13 15:45:49 +0700

    Initial commit

---
+++ airportmanagementsystem/src/main/java/com/airportmanagementsystem/airportmanagementsystem/AirportmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.airportmanagementsystem.airportmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportmanagementsystemApplication.class, args);
+	}
+
+}
